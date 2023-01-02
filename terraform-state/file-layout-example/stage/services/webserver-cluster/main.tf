09a4eb78f3bccb9eefe3d45c5c1ac72b15627346
baesinchul <chul9513@naver.com>
2023-01-02 14:44:02 +0900

    20230102

--- terraform-state/file-layout-example/stage/services/webserver-cluster/main.tf
+++ terraform-state/file-layout-example/stage/services/webserver-cluster/main.tf
@@ -1,6 +1,8 @@
 terraform {
+  # Terraform 버전 지정
   required_version = ">= 1.0.0, < 2.0.0"
 
+  #공급자 지정
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -9,17 +11,172 @@ terraform {
   }
 
   backend "s3" {
-    # 이전에 생성한 버킷 이름으로 변경
-    bucket = "std05-terraform-state"
-    key    = "global/s3/terraform.tfstate"
-    region = "ap-northeast-2"
-
-    # 이전에 생성한 다이나모DB 테이블 이름으로 변경
+    bucket         = "std05-terraform-state"
+    key            = "stage/services/webserver-cluster/terraform.tfstate"
+    region         = "ap-northeast-2"
     dynamodb_table = "std05-terraform-locks"
     encrypt        = true
   }
 }
-
 provider "aws" {
   region = "ap-northeast-2"
+}
+
+# 시작 템플릿 구성
+resource "aws_launch_template" "example" {
+  name                   = "std05-example"
+  image_id               = "ami-06eea3cd85e2db8ce"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  key_name               = "std05-key"
+
+  user_data = base64encode(data.template_file.web_output.rendered)
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+# 오토스케일링 그룹 생성
+resource "aws_autoscaling_group" "example" {
+  availability_zones = ["ap-northeast-2a", "ap-northeast-2c"]
+
+  name             = "std05-terraform-asg-example"
+  desired_capacity = 1
+  min_size         = 1
+  max_size         = 2
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "std05-terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+# 로드밸런서 생성
+resource "aws_lb" "example" {
+  name               = "std05-terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+# 대상그룹 생성
+resource "aws_lb_target_group" "asg" {
+  name     = "std05-terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# 로드밸런서 리스너 구성
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+# 로드밸런서 리스너 규칙 구성
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+# 인스턴스 보안그룹, 8080 오픈
+resource "aws_security_group" "instance" {
+  name = var.security_group_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 로드밸런서 보안그룹, 80 오픈
+resource "aws_security_group" "alb" {
+  name = "std05-terraform-example-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config = {
+    bucket = "std05-terraform-state"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "ap-northeast-2"
+  }
+}
+
+data "template_file" "web_output" {
+  template = file("${path.module}/web.sh")
+  vars = {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  }
 }
